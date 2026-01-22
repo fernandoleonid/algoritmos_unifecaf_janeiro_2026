8ae0ef62a5aaa5215bfa52337145aa1561b93cc2
Fernando Leonid <fernandoleonid@gmail.com>
2026-01-22 23:15:34 +0000

    lista e dicionários

--- live02/04.py
+++ live02/04.py
@@ -4,15 +4,23 @@ alunos = [
     {
         'nome': 'Ana',
         'idade': 22,
-        'nota': 8
+        'notas': [
+            4.0,
+            6.7,
+            9.0
+        ]
     },
     {
         'nome': 'Pedro',
         'idade': 44,
-        'nota': 6
+        'notas': [
+            7.0,
+            9.0,
+            9.5
+        ]
     }
 ]
 
 alunos [1]['nome'] = 'Pedo da Silva'
 
-print (alunos)
+print (alunos[1]['notas'][2])
